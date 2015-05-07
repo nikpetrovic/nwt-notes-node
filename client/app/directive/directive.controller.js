4ffbf79d19dd169e9f7da0768a56a43cd6753466
Nikola.Petrovic <Nikola.Petrovic@seavus.com>
2015-05-07 15:54:05 +0200

    deploy beautifying

--- client/app/directive/directive.controller.js
+++ client/app/directive/directive.controller.js
@@ -1,5 +1,12 @@
 'use strict';
 
 angular.module('nwtNotesApp').controller('DirectiveCtrl', function($scope, $http) {
-
+	$scope.menuLinks = [ {
+		text : '<i class=\'fa fa-pencil\'></i>&nbsp;Notes',
+		href : '/directive1'
+	}, {
+		text : '<i class=\"fa fa-download\"></i>&nbsp;Logout',
+		href : '/logout',
+		target : '_self'
+	} ];
 });
