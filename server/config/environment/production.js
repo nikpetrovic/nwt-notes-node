26533d7d133c5342dc70a10e2f235edbc43b72cd
Nikola.Petrovic <Nikola.Petrovic@seavus.com>
2015-05-08 15:29:17 +0200

    animate.css -> animamte-css

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,8 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/nwtnotes'
+            'mongodb://localhost/nwtnotes',
+    username : 'admin',
+    password : ' A64R4JvR2cH6'
   }
 };
