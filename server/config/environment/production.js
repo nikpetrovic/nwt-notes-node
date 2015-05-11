e5ec1b3fd13c47c750df169b4c051c2156f5fa22
Nikola.Petrovic <Nikola.Petrovic@seavus.com>
2015-05-11 16:38:24 +0200

    deploy config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,23 +3,17 @@
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+	// Server IP
+	ip : process.env.OPENSHIFT_NODEJS_IP || process.env.IP || undefined,
 
-  // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
+	// Server port
+	port : process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
 
-  // MongoDB connection options
-  mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/nwtnotes',
-    username : 'admin',
-    password : ' A64R4JvR2cH6'
-  }
+	// MongoDB connection options
+	mongo : {
+		uri : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.OPENSHIFT_MONGODB_DB_URL
+				+ process.env.OPENSHIFT_APP_NAME || 'mongodb://localhost/nwtnotes'
+	},
+	
+	seedDB: true
 };
